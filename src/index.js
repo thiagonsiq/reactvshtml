cda10e45e8319864b9d28306c1794c4f7320464c
Thiago Siqueira <thiagonsiq@gmail.com>
2022-04-08 10:05:24 -0400

    code for html vs react example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  document.getElementById('root').innerHTML = ` 
+    <div>
+      Hello HTML
+      <input />
+      <pre>${(new Date).toLocaleTimeString()}</pre>
+    </div>
+  `;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+    React.createElement(
+      'div', 
+      null, 
+      'Hello React',
+      React.createElement('input', null),
+      React.createElement('pre', null, (new
+      Date).toLocaleTimeString())
+    ),
+    document.getElementById('root2'),
+  );
+}
+
+setInterval(render, 1000);
